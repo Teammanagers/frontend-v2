269eefd320c0df23b3dc51d53c5c8ff187d2e319
Mincheol Park <69390311+Low-ProFiles@users.noreply.github.com>
2024-12-19 00:08:09 +0900

    docs: absolute path setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@/apis', replacement: '/src/apis' },
+      { find: '@/assets', replacement: '/src/assets' },
+      { find: '@/commons', replacement: '/src/commons' },
+      { find: '@/components', replacement: '/src/components' },
+      { find: '@/hooks', replacement: '/src/hooks' },
+      { find: '@/pages', replacement: '/src/pages' },
+      { find: '@/stores', replacement: '/src/stores' },
+      { find: '@/utils', replacement: '/src/utils' },
+    ],
+  },
+});
