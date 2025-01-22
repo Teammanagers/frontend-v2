fdf338ca297c3104e02fce8ae87ec579a175251a
김예안 <kjan0929@naver.com>
2025-01-22 14:39:30 +0900

    feat: Accordion 동작 로직 및 UI 구현

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { Accrodion } from '@/shared/components/accordion/Accordion';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Accordion</h2>
+
+      <Accrodion title="이예은" tagList={['기획', '프론트엔드', '백엔드']}>
+        <li>ToDo</li>
+        <li>ToDo</li>
+        <li>ToDo</li>
+        <li>ToDo</li>
+      </Accrodion>
+
+      <Accrodion title="이예은" tagList={['기획', '프론트엔드', '백엔드']}>
+        <li>ToDo</li>
+        <li>ToDo</li>
+        <li>ToDo</li>
+        <li>ToDo</li>
+      </Accrodion>
     </>
   );
 }
---
+++ src/shared/components/accordion/Accordion.tsx
@@ -0,0 +1,148 @@
+import { useRef, useState } from 'react';
+import styled from 'styled-components';
+import chevronIcon from '@/shared/assets/common/rotating-chevron.svg';
+
+interface AccrodionProps {
+  children?: React.ReactNode;
+  title: string;
+  tagList?: string[];
+}
+
+export const Accrodion = ({ children, title, tagList }: AccrodionProps) => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const parentRef = useRef<HTMLDivElement>(null);
+  const childRef = useRef<HTMLDivElement>(null);
+
+  const clickAccordionHandler = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+
+    // 열려있는 상태면 닫기
+    if (parentRef.current!.clientHeight > 0) {
+      parentRef.current!.style.height = '0';
+    }
+    // 닫혀있는 상태면 열기
+    else {
+      parentRef.current!.style.height = `${childRef.current!.clientHeight}px`;
+    }
+
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <AccordionContainer>
+      <AccordionHeader onClick={clickAccordionHandler} $isOpen={isOpen}>
+        <TitleWrapper>
+          <h3>{title}</h3>
+          <TagList>
+            {tagList?.map((tag, idx) => (
+              <Tag $isOpen={isOpen} key={`tag-${idx}`}>
+                {tag}
+              </Tag>
+            ))}
+          </TagList>
+        </TitleWrapper>
+
+        <IconWrapper $isOpen={isOpen}>
+          <img src={chevronIcon} alt="rotating-chevron-icon" />
+        </IconWrapper>
+      </AccordionHeader>
+
+      <AccordionBodyWrapper ref={parentRef}>
+        <AccordionBody ref={childRef} $isOpen={isOpen}>
+          {children}
+        </AccordionBody>
+      </AccordionBodyWrapper>
+    </AccordionContainer>
+  );
+};
+
+const AccordionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  width: 382px;
+  background-color: #ffffff;
+`;
+
+const AccordionHeader = styled.div<{ $isOpen: boolean }>`
+  box-sizing: border-box;
+  width: inherit;
+  height: 46px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 9px 15px;
+  border: 1px solid #f0f0f0;
+  border-radius: 6px;
+  background-color: ${({ $isOpen }) => ($isOpen ? '#F9FBFF' : '#ffffff')};
+  cursor: pointer;
+  box-shadow: ${({ $isOpen }) =>
+    $isOpen ? '0px 3px 9px rgba(0, 0, 0, 0.08)' : 'none'};
+
+  h3 {
+    font-size: 14px;
+    font-weight: 500;
+    color: #1d1d1d;
+  }
+`;
+
+const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+const TagList = styled.ul`
+  // ul 기본 속성 초기화
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  gap: 8px;
+`;
+
+const Tag = styled.li<{ $isOpen: boolean }>`
+  // li 기본 속성 초기화
+  box-sizing: border-box;
+  padding: 5px 8px;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 28px;
+  border-radius: 3px;
+  font-size: 12px;
+  font-weight: 500;
+  color: #5c9eff;
+  background-color: ${({ $isOpen }) => ($isOpen ? '#ffffff' : '#F9FBFF')};
+`;
+
+const IconWrapper = styled.button<{ $isOpen: boolean }>`
+  // button 기본 속성 초기화
+  background: none;
+  border: none;
+  padding: 0;
+  outline: none;
+  &:focus {
+    outline: none;
+  }
+  &:active {
+    outline: none;
+  }
+  transform: ${({ $isOpen }) => ($isOpen ? 'rotate(180deg)' : 'rotate(0deg)')};
+  transition: transform 0.5s;
+  cursor: pointer;
+`;
+
+const AccordionBodyWrapper = styled.div`
+  height: 0;
+  overflow: hidden;
+  transition: height 0.5s ease;
+`;
+
+const AccordionBody = styled.div<{ $isOpen: boolean }>`
+  height: 100px;
+  background-color: white;
+`;
