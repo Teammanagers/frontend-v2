bf3fcd37d3f7b183b1448aab61a20974b8d4defe
김예안 <kjan0929@naver.com>
2025-01-27 16:15:08 +0900

    chore: 수정/삭제 드롭다운 예시 코드 작성

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,66 @@
-import { useState } from 'react';
+import { Dropdown } from '@/shared/components/dropdown/Dropdown';
+import useToggle from '@/shared/components/dropdown/hooks/useToggle';
+import dropdownIcon from '@/shared/assets/icons/dropdown.svg';
+import styled from 'styled-components';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { isOpen, setIsOpen, toggle } = useToggle();
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <Dropdown setIsOpen={setIsOpen}>
+        <Dropdown.Trigger onClick={toggle}>
+          <img src={dropdownIcon} />
+        </Dropdown.Trigger>
+
+        <Dropdown.Menu isOpen={isOpen}>
+          <MenuWrapper>
+            <ModifyButton>수정</ModifyButton>
+            <DeleteButton>삭제</DeleteButton>
+          </MenuWrapper>
+        </Dropdown.Menu>
+      </Dropdown>
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MenuWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: -50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+  width: 55px;
+  height: 50px;
+  border-radius: 3px;
+  box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.1);
+
+  button {
+    width: 19px;
+    height: 15px;
+    font-size: 10px;
+    font-weight: 400;
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0;
+    cursor: pointer;
+  }
+`;
+
+const ModifyButton = styled.button``;
+
+const DeleteButton = styled.button`
+  color: #ff0000;
+`;
