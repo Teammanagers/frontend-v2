af7fcb0ddad83694d11fed42c3aa95a6b3de902e
zzzzzuuuuu <agu0830@naver.com>
2025-02-13 23:14:37 +0900

    style: 공통 색상 정의 및 ThemeProvider 설정

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Theme } from '@/app/styles/theme.ts';
 import App from './App';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 );
